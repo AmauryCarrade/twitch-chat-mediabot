ccb37dd3e9251aeedbf6d376d7b2a36f12b134da
Amaury Carrade <amaury@carrade.eu>
2019-12-19 16:49:12 +0100

    Avent 2019 : Luni-solaire & sources

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 const config = {
   'channel': 'eskimon', // Change here the name of your channel
   'cooldown': {
-    'user': 10,  // Minimal time (seconds) before a user can play again a track
-    'track': 10,  // Minimal time (seconds) before the same track can be played again
+    'user': 10,    // Minimal time (seconds) before a user can play again a track
+    'track': 10,   // Minimal time (seconds) before the same track can be played again
     'command': 1,  // Minimal time (seconds) before the command can be used again
   },
-  'prefix': '!play',  // Prefix to which your bot respond (ie. !play mysupermusic)
+  'commands': {
+    'play': '!play',  // Command to which your bot respond (e.g. !play mysupermusic)
+    'stop': '!stop'   // Command to which your bot stops the music when sent by a mod or the owner (e.g. !stop)
+  },
   'rights': {
     'user': true,  // Allow users to use the bot
     'mods': true,  // Allow moderators to use the bot
@@ -16,11 +19,11 @@ const config = {
 
 const tracks = [
   {
-    'command': 'alavolette',
+    'command': 'toast',
     'file': '/home/eskimon/Documents/Bot_twitch_audio/a_la_volette1.mp3'
   },
   {
-    'command': 'amoi',
+    'command': 'toast2',
     'file': '/home/eskimon/Documents/Bot_twitch_audio/a_moi.mp3'
   },
   {
